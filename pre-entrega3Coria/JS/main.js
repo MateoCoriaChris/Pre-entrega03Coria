6b66b4ee10fbb02b5eb7a32e82df8eb5d9a32523
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2023-02-06 17:00:37 -0300

    Update main.js

--- pre-entrega3Coria/JS/main.js
+++ pre-entrega3Coria/JS/main.js
@@ -1,48 +1,89 @@
 const productos = [
       {
         id: 1,
-        nombre: 'Producto 1',
+        nombre: 'Helado de 1kg',
         precio: 200,
         descripcion: 'Super producto 1 para el hogar',
         imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
-        categoria: 'categoria1'
+        categoria: 'Helados',
+        link:'productos/helados.html'
       },
       {
         id: 2,
-        nombre: 'Producto 2',
+        nombre: 'Helado de 1/2kg',
         precio: 1500,
         descripcion: 'Super producto 2 para el hogar',
         imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
-        categoria: 'categoria1'
+        categoria: 'Helados',
+        link:'productos/helados.html'
       },
       {
         id: 3,
-        nombre: 'Producto 3',
+        nombre: 'Helado de 1/4kg',
         precio: 2000,
         descripcion: 'Super producto 3 para el hogar',
         imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
-        categoria: 'categoria1'
+        categoria: 'Helados',
+        link:'productos/helados.html'
       },
       {
         id: 4,
-        nombre: 'Producto 4',
+        nombre: 'Pastel Cheesecake',
         precio: 500,
         descripcion: 'Super producto 4 para el hogar',
         imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
-        categoria: 'categoria2'
+        categoria: 'Pasteles',
+        link:'productos/pasteles.html'
       },
       {
         id: 5,
-        nombre: 'Producto 5',
+        nombre: 'Pastel Selva Negra',
         precio: 400,
         descripcion: 'Super producto 5 para el hogar',
         imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
-        categoria: 'categoria2'
-      }
+        categoria: 'Pasteles',
+        link:'productos/pasteles.html'
+      },
+      {
+        id: 6,
+        nombre: 'Chocotorta',
+        precio: 400,
+        descripcion: 'Super producto 5 para el hogar',
+        imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
+        categoria: 'Pasteles',
+        link:'productos/pasteles.html'
+      },
+      {
+        id: 7,
+        nombre: 'Lemonpie',
+        precio: 400,
+        descripcion: 'Super producto 5 para el hogar',
+        imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
+        categoria: 'Pasteles',
+        link:'productos/pasteles.html'
+      },
+      {
+        id: 8,
+        nombre: 'Bombon Suizo',
+        precio: 400,
+        descripcion: 'Super producto 5 para el hogar',
+        imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
+        categoria: 'categoria2',
+        link:'productos/postres'
+      },
+      {
+        id: 9,
+        nombre: 'Helados de Agua',
+        precio: 400,
+        descripcion: 'Super producto 5 para el hogar',
+        imagen: 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg',
+        categoria: 'Helados de agua',
+        link:'productos/heladosDeAgua.html'
+      },
     ];
-    
-    
-    /*const contenedorProductos = document.getElementById('contenedor-productos');
+  
+  
+    const contenedorProductos = document.getElementById('contenedor-productos');
     const carritoTexto = document.getElementById('carrito');
     const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
     const nuevaCantidadCarrito = carrito.reduce((acc, { cantidad }) => acc + cantidad, 0);
@@ -60,6 +101,7 @@ const productos = [
             <p>Categoria: ${producto.categoria}</p>
             <p>$ ${producto.precio}</p>
             <button class="btn btn-primary">Agregar al carrito</button>
+            <button type="button" class="btn btn-outline-danger btn-rounded" data-mdb-ripple-color="dark" id="Link" onclick="location.href='${producto.link}'" value="click here">enlace</button>
           </div>
         </div>
       `;
@@ -111,5 +153,3 @@ const productos = [
         });
       }
     );
-
-*/
